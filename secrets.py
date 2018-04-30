f6b81df39044d9cb9c65c3bfc5fe8f3b8d0be8ec
robertdooley <robert.dooley@records.nsw.gov.au>
2018-04-30 21:01:28 +1000

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
 consumer_key = k381VraCVG3kNNJjK0S0X01DG
 consumer_secret = 3nKHlZTU3UOflr19SLHxw2HowLomr2FZlN1gIlkyc4Ith7zjdz
 access_token = 968011625092415488-IvrC3I4QSbqXIodrciFP7BTfUJsEYVT
-access_token secret = sj8QJG9G8KPDIWfSA9BaW6ih5uFs9f2Vb7yxQI1YPLGPL
+access_token_secret = sj8QJG9G8KPDIWfSA9BaW6ih5uFs9f2Vb7yxQI1YPLGPL
